0fcb0a66a85138fcc35bf389ea2ad2ea795e2ec6
user <user@privatehost.netbynet.su>
2015-09-15 11:05:14 +0300

    urban thumb up

--- urban.pl
+++ urban.pl
@@ -6,6 +6,8 @@ use LWP::UserAgent;
 require HTTP::Response;
 use HTML::TreeBuilder;
 #use Getopt::Std;
+#Debian: liblwp-protocol-socks-perl
+use LWP::Protocol::socks;
 package main;
 #BAD CHARACTERS VERY RARELY
 
@@ -20,9 +22,12 @@ sub sig_handler { # подпрограмма-обработчик сигнала
 my $ua = LWP::UserAgent->new; #параметры подключения
 $ua->agent("Mozilla/5.0 (Windows NT 5.1; rv:5.0.1) Gecko/20100101 Firefox/5.0.1");
 #$ua->proxy(['http'], "http://127.0.0.1:4446");
+#$ua->proxy([qw(http https)] => "socks://172.16.0.1:9150"); #tor
+
 my $url="http://www.urbandictionary.com/define.php?term=".join ('+', @ARGV);
 my $req = HTTP::Request->new(GET => $url);
 my $res = $ua->request($req);
+#print $res->decoded_content;
 if ($res->is_success){
     my $tree = HTML::TreeBuilder->new; # обрабатываем первую страницу в дерево
     $tree->ignore_ignorable_whitespace(0);
@@ -35,8 +40,17 @@ if ($res->is_success){
     my $meaning = $el->look_down("class", "meaning")->as_text;
     if($meaning =~ m/There aren't any definitions for/){exit;}
     my $example = $el->look_down("class", "example")->as_text;
-    my $thumbup = $el->look_down("class", "thumb up")->look_down("class", "count")->as_text;
-    my $thumbdown = $el->look_down("class", "thumb down")->look_down("class", "count")->as_text;
+    my $thumbup = $el->look_down("class", "up");
+    if (! $thumbup){
+	$thumbup = $el->look_down("class", "thumb up");
+    }
+    $thumbup = $thumbup->look_down("class", "count")->as_text;
+        
+    my $thumbdown = $el->look_down("class", "down");
+    if (! $thumbdown){
+	$thumbdown = $el->look_down("class", "thumb down");
+    }
+    $thumbdown = $thumbdown->look_down("class", "count")->as_text;
     if($thumbup < 20 || $thumbup > 9000 || ($thumbup-$thumbdown) < 13 ){exit;} #silent exit if too many ppl like it
     $meaning =~ s/^\s+|\s+$//g;     #trim both sides
 	#$meaning =~ s/[#\-%&\$*+()]//g; #remove bad characters
@@ -46,7 +60,7 @@ if ($res->is_success){
     print "Urban ".$thumbup."/".$thumbdown.":\n";
     print $meaning."\n";
     print $example."\n";
-#    $elr->dump;
+    #$tree->dump;
 }else{print (($res->status_line)." Can't connect urban.\n")};
 
 
